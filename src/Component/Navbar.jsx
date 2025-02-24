1861366d38c441ad163ef6b2cdd648441761f7ba
kajal-0s <sawkajal28@gmail.com>
2025-02-24 22:14:28 +0530

    updated project

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -6,22 +6,22 @@ function Navbar() {
 
     
     <header className='header' >
-        <nav className="flex text-lg ml-220 font-medium bg-transparent p-3  space-x-30">
-        <NavLink to="/" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
-        <p className=' hover:text-violet-700 h-9 w-15 rounded-xl p-1 bg-white border-rose-900 border-1  '>Home</p>
+        <nav className="flex text-lg ml-210 font-medium bg-transparent p-3  space-x-30">
+        <NavLink to="/" className={({isActive}) => isActive ? 'text-black' : 'text-white'}>
+        <p className=' hover:text-violet-700 h-9 w-15 rounded-xl p-1 bg-red-900 border-rose-900 border-1  '>Home</p>
         </NavLink>
         
-            <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
-            <p className=' hover:text-violet-700 h-9 w-15 rounded-xl p-1 bg-white border-rose-900 border-1     '>About</p> 
+            <NavLink to="/about" className={({isActive}) => isActive ? 'text-black' : 'text-white'}>
+            <p className=' hover:text-violet-700 h-9 w-15 rounded-xl p-1 bg-red-900 border-rose-900 border-1     '>About</p> 
             </NavLink>
 
-            <NavLink to="/projects" className={({isActive}) => isActive ? 'text-blue-500': 'text-black'}>
-            <p className=' hover:text-violet-700 h-9 w-17 rounded-xl p-1 bg-white border-rose-900 border-1  '>Project</p> 
+            <NavLink to="/projects" className={({isActive}) => isActive ? 'text-black': 'text-white'}>
+            <p className=' hover:text-violet-700 h-9 w-17 rounded-xl p-1 bg-red-900 border-rose-900 border-1  '>Project</p> 
             </NavLink>
 
            
-            <NavLink to="/contact" className={({isActive}) => isActive ? 'text-blue-500': 'text-black'}>
-            <p className=' hover:text-violet-700 h-9 w-19 rounded-xl p-1 bg-white border-rose-900 border-1  '>Contact</p>
+            <NavLink to="/contact" className={({isActive}) => isActive ? 'text-black': 'text-white'}>
+            <p className=' hover:text-violet-700 h-9 w-19 rounded-xl p-1 bg-red-900 border-rose-900 border-1  '>Contact</p>
             </NavLink>
 
         </nav>
